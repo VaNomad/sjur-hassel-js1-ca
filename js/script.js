a471f7ee75dc1bc3d536a5e1dbe291d34def3aa5
VaNomad <sjur.hassel@gmail.com>
2022-05-03 11:29:27 +0100

    changing div layout and some styling

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 
 const url = "https://gateway.marvel.com/v1/public/characters?apikey=bbf15e68860571493abe4a781bdf7e71"
 
-const resultsContainer = document.querySelector(".main-container");
+const secondContainer = document.querySelector(".secondContainer");
 
 async function callApi() {
   try {
@@ -10,12 +10,21 @@ async function callApi() {
     const json = await response.json();
     const data = json.data.results;
 
-    resultsContainer.innerHTML = "";
+    secondContainer.innerHTML = "";
 
     for (let i = 0; i < data.length; i++) {
-      console.log(data[i].name);
+      console.log(data[i]);
 
-      resultsContainer.innerHTML = `<div style="font-size: 3rem;">seriously? ${data[0].name} ?</div>`;
+      if (data[i] === 3) {
+        break;
+      }
+
+      secondContainer.innerHTML += `
+      <div class="results-container">
+        <h2 style="font-size: 3rem;">Name:  ${data[i].name}</h2>
+        <h3 style="font-size: 2rem;">ID:  ${data[i].id}</h3>
+        <p style="font-size: 2rem;">Modified: ${data[i].modified}</p>
+      </div>`;
     }
 
   } catch (error) {
