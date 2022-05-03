7e449ec2f2a125a8038c528c0a7d6bb28c409377
VaNomad <sjur.hassel@gmail.com>
2022-05-03 16:29:19 +0100

    trying to add img from thumbnail, not working

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
-
 const url = "https://gateway.marvel.com/v1/public/characters?apikey=bbf15e68860571493abe4a781bdf7e71"
 
-const secondContainer = document.querySelector(".secondContainer");
+const container = document.querySelector(".container");
 
 async function callApi() {
   try {
@@ -10,20 +9,17 @@ async function callApi() {
     const json = await response.json();
     const data = json.data.results;
 
-    secondContainer.innerHTML = "";
+    container.innerHTML = "";
 
     for (let i = 0; i < data.length; i++) {
       console.log(data[i]);
 
-      if (data[i] === 3) {
-        break;
-      }
-
-      secondContainer.innerHTML += `
+      container.innerHTML += `
       <div class="results-container">
-        <h2 style="font-size: 3rem;">Name:  ${data[i].name}</h2>
-        <h3 style="font-size: 2rem;">ID:  ${data[i].id}</h3>
+        <h2 style="font-size: 3.5rem;">Name: ${data[i].name}</h2>
+        <h3 style="font-size: 2rem;">ID: ${data[i].id}</h3>
         <p style="font-size: 2rem;">Modified: ${data[i].modified}</p>
+        <div class="details-image" style="background-image: url("${data[i].thumbnail}")"></div>
       </div>`;
     }
 
