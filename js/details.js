d02ea5265340dc6056ddc48f8eb0bc133bbd7050
VaNomad <sjur.hassel@gmail.com>
2022-05-05 12:42:23 +0100

    finding typo in url, finally, aaaaargh!

--- js/details.js
+++ js/details.js
@@ -12,8 +12,6 @@ console.log(id);
 const apikey = "?apikey=bbf15e68860571493abe4a781bdf7e71"
 const url = "https://gateway.marvel.com/v1/public/characters/" + id + apikey;
 
-// const wholeUrl = "https://gateway.marvel.com/v1/public/characters/1011031?apikey=bbf15e68860571493abe4a781bdf7e71";
-
 async function getCharacter() {
   try {
     const response = await fetch(url);
@@ -27,21 +25,14 @@ async function getCharacter() {
     for (let i = 0; i < data.length; i++) {
       console.log(data[i]);
       
-
       details.innerHTML = `
         <h1>${data[i].name}</h1>
         <h2>${data[i].id}<h2>
-        <h3>${data[i].modified}<h3>
-        <div style="background-image: url(${data[i].path});"></div>`;
+        <h3>${data[i].description}<h3>`;
     }
-  
-    
   } catch (error) {
       console.log(error);
-      details.innerHTML = ("There seems to be an error", error)
-      
+      details.innerHTML = ("There seems to be an error", error) 
   }
-  
-
 }
 getCharacter();
--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 const container = document.querySelector(".container");
 
 const apikey = "?apikey=bbf15e68860571493abe4a781bdf7e71"
-const url = "https://gateway.marvel.com/v1/public/characters/" + apikey;
+const url = "https://gateway.marvel.com/v1/public/characters" + apikey;
 
 async function callApi() {
   try {
@@ -21,21 +21,8 @@ async function callApi() {
         <h2 style="font-size: 3.5rem;">Name: ${character.name}</h2>
         <h3 style="font-size: 2rem;">ID: ${character.id}</h3>
         <p style="font-size: 2rem;">Modified: ${character.modified}</p>
-        <div style="background-image: url(${character.thumbnail});"></div>
       </a>`;
     });
-
-    {/* for (let i = 0; i < characters.length; i++) {
-      console.log(characters[i]);
-
-      container.innerHTML += `
-                             <a href="details.html?id=${characters.id}" class="character-cards">
-                               <h2 style="font-size: 3.5rem;">Name: ${characters[i].name}</h2>
-                               <h3 style="font-size: 2rem;">ID: ${characters[i].id}</h3>
-                               <p style="font-size: 2rem;">Modified: ${characters[i].modified}</p>
-                               <<img src="${characters[i].thumbnail}">>
-                             </a>`;
-    } */}
 
   } catch (error) {
     console.log(error);
@@ -43,4 +30,3 @@ async function callApi() {
   }
 }
 callApi();
-
