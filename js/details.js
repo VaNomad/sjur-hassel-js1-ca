f27c1febc4e268a1f06d7f21fe5deb79f9618136
VaNomad <sjur.hassel@gmail.com>
2022-05-05 07:33:37 +0100

    query string targeting

--- js/details.js
+++ js/details.js
@@ -9,18 +9,34 @@ const id = params.get("id");
 console.log(id);
 
 
-const url = "https://gateway.marvel.com/v1/public/characters/1011031?apikey=bbf15e68860571493abe4a781bdf7e71";
+
+const endpoint = "https://gateway.marvel.com/v1/public/characters/1011031"
+const apikey = "?apikey=bbf15e68860571493abe4a781bdf7e71"
+const url = endpoint + id + apikey;
+
+// const arl = "https://gateway.marvel.com/v1/public/characters/1011031?apikey=bbf15e68860571493abe4a781bdf7e71";
 
 async function getCharacter() {
   try {
     const response = await fetch(url);
     const json = await response.json();
+    const data = json.data.results;
   
-    console.log(json);
+    console.log(data);
 
     details.innerHTML = "";
+
+    for (let i = 0; i < data.length; i++) {
+      console.log(data[i]);
+      
+
+      details.innerHTML = `
+        <h1>${data[i].name}</h1>
+        <h2>${data[i].id}<h2>
+        <h3>${data[i].modified}`;
+    }
   
-    details.innerHTML = `<h1>${json.name}</h1>`;
+    
   } catch (error) {
       console.log(error);
       details.innerHTML = ("There seems to be an error", error)
